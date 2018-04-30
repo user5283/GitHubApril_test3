2eb9c64e47728c6a839c3d2c5d7a075edb331042
Clera <Clera@138.79.113.100>
2018-04-30 13:52:05 +1200

    First commit.

--- JavaApplication19/src/ticketingSystem/TicketingSystem.java
+++ JavaApplication19/src/ticketingSystem/TicketingSystem.java
@@ -25,6 +25,8 @@ public class TicketingSystem {
         public void run() {
                 
             System.out.println("Sales Assistant is ready to see the next customer."); 
+             System.out.println("Sales Assistant is ready to see the next customer.");
+              System.out.println("Sales Assistant is ready to see the next customer.");
             
             /*the rest of code to display the next customer in line, what number
             ticket is next in line and list all customers in a queue */
